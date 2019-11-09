7e6b3839b7efde80d2fdf168b792890cfb0ee164
axel1234567890 <57529766+axel1234567890@users.noreply.github.com>
2019-11-09 14:33:30 +0100

    Update zal.c

--- zal.c
+++ zal.c
@@ -22,5 +22,85 @@ void afficherRueSavidanVide(){
 }
 
 int aleatoire(int limite){
-    returnrand() %limite;
+    return rand() %limite;
 }
+/** 
+Fonction qui initialise une nouvelle structure de type héros
+. Un héros est par défaut positionné en X à LARGEUR_RUE / 2 et à Y = LONGUEUR_RUE, où LARGEUR_RUE et 
+LONGUEUR_RUE sont des constantes symboliques définies dans zal.h.
+*@return Heros une structure de type Heros
+*/
+struct heros initialiserHeros(){
+	struct heros Jako = { LARGEUR_RUE/2, LONGUEUR_RUE, 'H' ,};
+	return Jako;
+	};
+/** 
+Fonction qui initialise une nouvelle structure de type zombi.Un zombiest par défaut 
+positionné en haut de la rue Jean Savidan, posY = 0 et aléatoirement en X
+*@return Zombiune structure de type Zombi
+*/
+struct zombi initialiserZombi(){
+	 int i = aleatoire(LARGEUR_RUE);
+	struct zombi Z1 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z2 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z3 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z4 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z5 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z6 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z7 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z8 = {i , 0, 'Z' ,};
+	 i = aleatoire(LARGEUR_RUE);
+	struct zombi Z9 = {i , 0, 'Z' ,};
+	
+	struct zombi Z0 = {i , 0, 'Z' ,};
+	return Z1, Z2, Z3, Z4, Z5, Z6, Z7, Z8, Z9, Z0;
+}
+/** 
+Procédure qui initialise un tableau de zombis passé en paramètre. Ce tableau est de 
+type TabZombis (voir étape) et est directement modifié dans la procédure (normal, on ne 
+peut pas retourner de tableau depuis une fonction).
+*@
+param
+TabZombis un tableau de 
+NBZOMBIS structures de type zombi
+*/
+void afficherPosH(struct heros Jako){
+	
+	printf("Votre héros jako est en vie et se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Jako.posx, Jako.posy);
+	}
+	
+void afficherPosZ1(struct zombi Z1){ printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z1.zposx, Z1.zposy);
+	}
+	void afficherPosZ2(struct zombi Z2){
+			printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z2.zposx, Z2.zposy);
+		}
+	void afficherPosZ3(struct zombi Z3){ printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z3.zposx, Z3.zposy);
+}
+	void afficherPosZ4(struct zombi Z4){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z4.zposx, Z4.zposy);
+}
+	void afficherPosZ5(struct zombi Z5){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z5.zposx, Z5.zposy);
+}
+
+	void afficherPosZ6(struct zombi Z6){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z6.zposx, Z6.zposy);
+
+}
+	void afficherPosZ7(struct zombi Z7){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z7.zposx, Z7.zposy);
+}
+	void afficherPosZ8(struct zombi Z8){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z8.zposx, Z8.zposy);
+}
+	void afficherPosZ9(struct zombi Z9){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z9.zposx, Z9.zposy);
+}
+	void afficherPosZ0(struct zombi Z0){	printf("un zombie se trouve aux coordonnées posX= %d et posY= %d de la rue Jean Savidan.\n", Z0.zposx, Z0.zposy);
+}
+		
+	
+
+
+
