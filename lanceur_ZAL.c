cbb249bf19e9ed5d3b7815b28c19c0c89ca3da85
axel1234567890 <57529766+axel1234567890@users.noreply.github.com>
2019-11-09 11:24:18 +0100

    Update lanceur_ZAL.c

--- lanceur_ZAL.c
+++ lanceur_ZAL.c
@@ -4,11 +4,17 @@
 //A compiler : gcc -W -Wall *.c -o zal
 
 int main(){
-	srand(time(NULL));
-	struct heros { int posx; int posy;};
-	struct zombi { int zposx; int zposy; int tabZombi[NBZOMBIS]};
     srand(time(NULL));
-  
     afficherRueSavidanVide();
     return 0; 
+    
+ 
+    
+
+    
+
+
+
+    
+    
 }
