6cdd9c8865cc2a196c3d026d638300bf6b9d8709
axel1234567890 <57529766+axel1234567890@users.noreply.github.com>
2019-11-11 11:15:28 +0100

    Update lanceur_ZAL.c

--- lanceur_ZAL.c
+++ lanceur_ZAL.c
@@ -1,43 +1,12 @@
 #include "zal.h"
 
-//Code source principal
-//A compiler : gcc -W -Wall *.c -o zal
-
 int main(){
     srand(time(NULL));
-    afficherRueSavidanVide();
-    struct heros jako = initialiserHeros();
-    struct zombi Z1 = initialiserZombi();
-    struct zombi Z2 = initialiserZombi();
-    struct zombi Z3 = initialiserZombi();
-    struct zombi Z4 = initialiserZombi();
-    struct zombi Z5 = initialiserZombi();
-    struct zombi Z6 = initialiserZombi();
-    struct zombi Z7 = initialiserZombi();
-    struct zombi Z8 = initialiserZombi();
-    struct zombi Z9 = initialiserZombi();
-    struct zombi Z0 = initialiserZombi();
-    afficherPosH(jako);
-    afficherPosZ1(Z1);
-    afficherPosZ1(Z2);
-    afficherPosZ1(Z3);
-    afficherPosZ1(Z4);
-    afficherPosZ1(Z5);
-    afficherPosZ1(Z6);
-    afficherPosZ1(Z7);
-    afficherPosZ1(Z8);
-    afficherPosZ1(Z9);
-    afficherPosZ1(Z0);
-
+	afficherRueSavidanVide();
+	tabzombis zombis;
+	initialiserHeros(); 
+	initialiserZombis(zombis);
+	infoHeros(heros);
+	infoZombiAll(zombis); 
     return 0; 
-    
- 
-    
-
-    
-
-
-
-    
-    
 }
